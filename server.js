2017968823a8afe9c428cf09254c760b8880d1fd
chuanluu <chuanluu0103@gmail.com>
2021-10-25 15:57:00 +0700

    deoloyV1

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -29,10 +30,12 @@ mongoose.connect(URI, (err) => {
   console.log("Connect to MongoDB success");
 });
 
-// Test connect to db
-// app.get("/", (req, res) => {
-//   res.json({ msg: "Welcome my bookstore CHUANG CHUANG" });
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
